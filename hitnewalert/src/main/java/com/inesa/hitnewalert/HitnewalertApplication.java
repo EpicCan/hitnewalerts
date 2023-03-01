62eff4f6c06df2fb7e6dc2b5b06410efcb904c6f
can <769658249@qq.com>
2023-03-01 15:48:01 +0800

    打新提醒

---
+++ hitnewalert/src/main/java/com/inesa/hitnewalert/HitnewalertApplication.java
@@ -0,0 +1,13 @@
+package com.inesa.hitnewalert;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HitnewalertApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HitnewalertApplication.class, args);
+    }
+
+}
---
+++ hitnewalert/src/test/java/com/inesa/hitnewalert/HitnewalertApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inesa.hitnewalert;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HitnewalertApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
